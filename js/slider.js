539ff8c2fc6bd02ca05fdcf1b32b42cf61bf52dd
tomatofrog1 <alcosharley21@gmail.com>
2024-10-10 15:46:08 +0800

    first commit

---
+++ js/slider.js
@@ -0,0 +1,12 @@
+var slider = tns({
+  container: ".slider__images",
+  items: 6,
+  speed: 200,
+  gutter: 1,
+  slideBy: "1",
+  autoplay: true,
+  navPosition: "top",
+  containerControls: "#controls",
+  prevButton: ".prev",
+  nextButton: ".next",
+});
